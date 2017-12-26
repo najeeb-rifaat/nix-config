a91bf46465b2182cadfac015e47f7093d4cc739d
Najeeb Rifaat <najeeb@mckinsey.com>
2017-12-26 21:31:59 +0800

    chore(auto): automate linking with [link] script

--- link.sh
+++ link.sh
@@ -2,4 +2,5 @@
 
 ln -sfn $PWD/.tmux.conf ~/.tmux.conf
 ln -sfn $PWD/init.vim ~/.config/nvim/init.vim
-ln -sfn $PWD/spell ~/.config/nvim/spell
+ln -sfn $PWD/spell/en.utf-8.add ~/.config/nvim/spell/en.utf-8.add
+ln -sfn $PWD/spell/en/utf-8.add.spl ~/.config/nvim/spell/en.utf-8.add.spl
