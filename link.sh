5ef4517d2b1e4c3cccd2bc20829128268cd99a74
Najeeb Rifaat <najeeb_rifaat@mckinsey.com>
2018-01-10 00:43:39 +0700

    chore(cleanup): cleanup and improve installer

--- link.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-ln -sfn $PWD/.tmux.conf ~/.tmux.conf
-ln -sfn $PWD/init.vim ~/.config/nvim/init.vim
-ln -sfn $PWD/spell/en.utf-8.add ~/.config/nvim/spell/en.utf-8.add
-ln -sfn $PWD/spell/en/utf-8.add.spl ~/.config/nvim/spell/en.utf-8.add.spl
