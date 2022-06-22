988f86e1d7765acd6a9d02677b8ed9388f395542
Najeeb R <mohamme89d@gmail.com>
2022-06-22 18:39:39 +0800

    chore(sync): sync latest changes

---
+++ coc-config.js
@@ -0,0 +1,13 @@
+{
+  "python.jediEnabled": false,
+  "java.jdt.ls.vmargs": "-javaagent:/home/najee89b/.m2/repository/org/projectlombok/lombok/1.18.22/lombok-1.18.22.jar",
+  "svelte.enable-ts-plugin": true,
+  "languageserver": {
+    "csharp-ls": {
+      "command": "csharp-ls",
+      "filetypes": ["cs"],
+      "rootPatterns": ["*.csproj", ".vim/", ".git/", ".hg/"]
+    }
+  },
+  "java.errors.incompleteClasspath.severity": "ignore"
+}
